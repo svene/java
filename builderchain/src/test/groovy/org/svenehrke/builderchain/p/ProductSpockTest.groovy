4438261ba07b2eac159c7ba18d00f190934d8924
svene <sven.ehrke@canoo.com>
2011-03-25 16:51:04 +0100

    introduced Spock Test for 'Product' (mainly to test compilation-failing of invalid construction paths)

---
+++ builderchain/src/test/groovy/org/svenehrke/builderchain/p/ProductSpockTest.groovy
@@ -0,0 +1,49 @@
+package org.svenehrke.builderchain.p
+
+import spock.lang.Specification
+
+class ProductSpockTest extends Specification {
+
+	def setup() {
+	}
+
+	def	cleanup() {
+	}
+
+	def "verify that builder works with required arguments"() {
+		given:
+		when:
+			Product p = Product.withRequired1("r1").withRequired2("r2").withRequired3("r3").build()
+		then:
+			"r1" == p.getRequired1()
+			"r2" == p.getRequired2()
+			"r3" == p.getRequired3()
+	}
+
+	def "verify that builder works with required and optional arguments"() {
+		given:
+		when:
+			Product p = Product.withRequired1("r1").withRequired2("r2").withRequired3("r3").andOptional1("o1").andOptional2("o2").andOptional3("o3").build()
+		then:
+			"r1" == p.getRequired1()
+			"r2" == p.getRequired2()
+			"r3" == p.getRequired3()
+			"o1" == p.getOptional1()
+			"o2" == p.getOptional2()
+			"o3" == p.getOptional3()
+	}
+
+	def "verify that invalid construction paths would not be compilable under Java"() {
+		given:
+		when:
+			Product.andOptional1("o1").build()
+		then:
+			thrown(MissingMethodException)
+
+		when:
+			Product.withRequired2("r2")
+		then:
+			thrown(MissingMethodException)
+	}
+
+}
