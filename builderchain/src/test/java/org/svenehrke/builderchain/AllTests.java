774bf18724daa9ee06021b0e5930a800ec5e76bd
svene <sven.ehrke@canoo.com>
2011-03-25 16:49:23 +0100

    adapted AllTests.java

--- builderchain/src/test/java/org/svenehrke/builderchain/AllTests.java
+++ builderchain/src/test/java/org/svenehrke/builderchain/AllTests.java
@@ -8,7 +8,6 @@ package org.svenehrke.builderchain;
 
 import org.svenehrke.builderchain.v1.NutritionFacts1Test;
 import org.svenehrke.builderchain.v2.NutritionFacts2Test;
-import org.svenehrke.builderchain.v3.NutritionFacts3aTest;
 import org.svenehrke.builderchain.v3.NutritionFacts3bTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +20,6 @@ import org.svenehrke.builderchain.v4.V4Test;
 @SuiteClasses( {
 	NutritionFacts1Test.class
 	, NutritionFacts2Test.class
-	, NutritionFacts3aTest.class
 	, NutritionFacts3bTest.class
 	, NutritionFacts3cTest.class
 	, V4Test.class
