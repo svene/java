8307185e98561a9fb22caead22dfeb4931b6c3c0
svene <sven.ehrke@canoo.com>
2011-12-29 11:33:21 +0100

    made it a real test

--- misc/src/test/java/org/svenehrke/java/misc/enummap/EnumMapTest.java
+++ misc/src/test/java/org/svenehrke/java/misc/enummap/EnumMapTest.java
@@ -4,13 +4,19 @@ import org.junit.Test;
 
 import java.util.EnumMap;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class EnumMapTest {
 	@Test
 	public void test1() throws Exception {
-		final EnumMap<DBKey,String> enumMap = DBKey.load(EnumMapTest.class.getResourceAsStream("db_connection.properties"));
-		for (DBKey key : enumMap.keySet()) {
-			System.out.printf("%s: %s%n", key.name(), enumMap.get(key));
-		}
-
+		final EnumMap<DBKey,String> enumMap = DBKey.load(EnumMapTest.class.getResourceAsStream("/enummap/db_connection.properties"));
+		assertTrue(enumMap.containsKey(DBKey.USER));
+		assertEquals("admin", enumMap.get(DBKey.USER));
+		assertTrue(enumMap.containsKey(DBKey.PASSWORD));
+		assertTrue(enumMap.containsKey(DBKey.URL));
+		assertTrue(enumMap.containsKey(DBKey.MIN_CONNECTION));
+		assertTrue(enumMap.containsKey(DBKey.MAX_CONNECTION));
 	}
 }
