a15398a49999128e5642e0ab233eb8e89297a316
svene <sven.ehrke@canoo.com>
2011-12-30 17:23:41 +0100

    first steps with jax ws 2

--- misc/src/test/java/org/svenehrke/java/misc/ws/Main.java
+++ misc/src/test/java/org/svenehrke/java/misc/ws/Main.java
@@ -1,4 +1,25 @@
 package org.svenehrke.java.misc.ws;
 
+import javax.xml.ws.Endpoint;
+
+
+/**
+ * after compilation run in '.../misc/build/classes/test':
+ *
+ *   wsgen -cp . org.svenehrke.java.misc.ws.CircleFunction
+ *
+ *
+ *
+ * Run this main.
+ * Then point the browser to:
+ *   http://localhost:8080/WebServiceExample/circlefunctions?WSDL
+ *
+ * TODO: find out how a client uses the webservice (soapui?)
+ *
+ *
+ */
 public class Main {
+	public static void main(String[] args) {
+		Endpoint.publish("http://localhost:8080/WebServiceExample/circlefunctions", new CircleFunctions());
+	}
 }
